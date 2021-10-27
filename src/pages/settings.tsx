30f242b036f8500ef5859866f040a69d5d088c57
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-27 06:38:11 -0300

    Side menu with icons.

---
+++ src/pages/settings.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+export default function Settings() {
+  return (
+    <Layout title="Settings" 
+       subtitle="Manage your settings.">
+      <h3>Content</h3>
+    </Layout>
+  )
+}
