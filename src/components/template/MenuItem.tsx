524c1bfc4a66291f821a183f388b7148faced565
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-27 07:08:27 -0300

    Logout button and new menu items.

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,24 +1,39 @@
 import Link from 'next/link'
 
 interface MenuItemProps {
-    url: string
+    url?: string
     text: string
     icon: any
+    className?: string
+    onClick?: (event: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
-    return (
-        <li className={`hover:bg-gray-100`}>
-            <Link href={props.url}>
-                <a className={`
+    function renderLink() {
+        return (
+            <a className={`
                     flex flex-col justify-center 
-                    items-center h-20 w-full w-20`}>
-                    {props.icon}    
-                <span className={`text-xs font-light text-gray-600`}>
-                    {props.text}   
+                    items-center h-20 w-20 text-gray-600 ${props.className}
+                `}>
+                {props.icon}
+                <span className={`text-xs font-light`}>
+                    {props.text}
                 </span>
-                </a>
-            </Link>
+            </a>
+        )
+    }
+    return (
+        <li onClick={props.onClick} className={`
+            hover:bg-gray-100
+            cursor-pointer
+        `}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderLink()}
+                </Link>
+            ) : (
+                renderLink()
+            )}
         </li>
     )
 }
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,4 +1,4 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../icons";
+import { BellIcon, HomeIcon, LogOutIcon, SettingsIcon } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
@@ -6,18 +6,28 @@ import MenuItem from "./MenuItem";
 export default function SideMenu() {
 
     return (
-        <aside>
+        <aside className="flex flex-col">
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 to-purple-800
                 h-20 w-20 
             `}>
-                <Logo/>
+                <Logo />
             </div>
+            <ul className="flex-grow">
+                <MenuItem url="/" text="Home" icon={HomeIcon} />
+                <MenuItem url="/settings" text="Settings" icon={SettingsIcon} />
+                <MenuItem url="/notifications" text="Notifications" icon={BellIcon} />
+            </ul>
             <ul>
-                <MenuItem url="/" text="Home" icon={HomeIcon}/>
-                <MenuItem url="/settings" text="Settings" icon={SettingsIcon}/>
-                <MenuItem url="/notifications" text="Notifications" icon={BellIcon}/>
+                <MenuItem
+                    onClick={() => console.log('logout')}
+                    text="LogOut" icon={LogOutIcon}
+                    className={`
+                        text-red-600
+                        hover:bg-red-400 hover:text-white
+                    `}
+                />
             </ul>
         </aside>
     )
