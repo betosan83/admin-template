2139b6bce3b0e9548d51d6b397dae7c2a7938766
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-28 20:44:58 -0300

    Auth page stylized.

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,19 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
     return props.doNotRender ? null : (
-        <div className={`flex flex-col`}>
+        <div className={`flex flex-col mt-4`}>
             <label>{props.label}</label>
             <input 
                 type={props.type ?? 'text'}
                 value={props.value} 
                 onChange={e => props.valueChanged?.(e.target.value)}
-                required={props.required}/>
+                required={props.required}
+                className={`
+                    px-4 py-3 rounded-lg bg-gray-200 mt-2
+                    border focus:border-blue-500
+                    focus:bg-white
+                    focus:outline-none
+                `}/>
         </div>
     )
 }
--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -7,41 +7,52 @@ export default function Authentication() {
     const [password, setPassword] = useState('')
 
     function submit() {
-        if(mode === 'login') {
+        if (mode === 'login') {
             console.log('login')
         } else {
             console.log('signup')
         }
     }
     return (
-        <div>
-            <h1 className={`
+        <div className="flex flex-col h-screen items-center justify-center">
+            <div className="w-1/2">
+                <h1 className={`
                 text-xl font-bold mb-5
             `}>
-                {mode === 'login' ? 'Inform your E-mail and Password' : 'Sign Up!'}
-            </h1>
-            <AuthInput
-                label="E-mail"
-                type="email"
-                value={email}
-                required
-                valueChanged={setEmail}
-            />
-            <AuthInput
-                label="Password"
-                type="password"
-                value={password}
-                required
-                valueChanged={setPassword}
-            />
+                    {mode === 'login' ? 'Inform your E-mail and Password' : 'Sign Up!'}
+                </h1>
+                <AuthInput
+                    label="E-mail"
+                    type="email"
+                    value={email}
+                    required
+                    valueChanged={setEmail}
+                />
+                <AuthInput
+                    label="Password"
+                    type="password"
+                    value={password}
+                    required
+                    valueChanged={setPassword}
+                />
 
-            <button onClick={submit} className={`
-                w-full bg-indigo-500 hover:bg-indigo-400
-                text-white rounded-lg px-4 py-3 mt-6
-            `}>
-                {mode === 'login' ? 'Log in' : 'Sign Up'}
-            </button>
+                <button onClick={submit} className={`
+                    w-full bg-indigo-500 hover:bg-indigo-400
+                    text-white rounded-lg px-4 py-3 mt-6
+                `}>
+                    {mode === 'login' ? 'Log in' : 'Sign Up'}
+                </button>
 
+                <hr className="my-6 border-gray-300 w-full" />
+
+                <button onClick={submit} className={`
+                    w-full bg-red-500 hover:bg-red-400
+                    text-white rounded-lg px-4 py-3
+                `}>
+                    Join with Google!
+                </button>
+            </div>
         </div>
+
     )
 }
