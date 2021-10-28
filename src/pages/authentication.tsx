64144cb871c9b6cdff87a976c4d2d333a49f9e13
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-28 20:00:31 -0300

    New authentication page.

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -2,23 +2,45 @@ import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
 
 export default function Authentication() {
+    const [mode, setMode] = useState<'login' | 'register'>('login')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    function submit() {
+        if(mode === 'login') {
+            console.log('login')
+        } else {
+            console.log('signup')
+        }
+    }
     return (
         <div>
-            <h1>Authentication</h1>
+            <h1 className={`
+                text-xl font-bold mb-5
+            `}>
+                {mode === 'login' ? 'Inform your E-mail and Password' : 'Sign Up!'}
+            </h1>
             <AuthInput
                 label="E-mail"
                 type="email"
                 value={email}
+                required
                 valueChanged={setEmail}
             />
             <AuthInput
                 label="Password"
                 type="password"
                 value={password}
+                required
                 valueChanged={setPassword}
             />
+
+            <button onClick={submit} className={`
+                w-full bg-indigo-500 hover:bg-indigo-400
+                text-white rounded-lg px-4 py-3 mt-6
+            `}>
+                {mode === 'login' ? 'Log in' : 'Sign Up'}
+            </button>
 
         </div>
     )
