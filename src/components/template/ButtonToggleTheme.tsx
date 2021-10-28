9c9fc368a87f2d00160e7aeb3895cecb30b72022
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-28 07:51:11 -0300

    Theme button styles.

--- src/components/template/ButtonToggleTheme.tsx
+++ src/components/template/ButtonToggleTheme.tsx
@@ -1,4 +1,4 @@
-import { SunIcon } from "../icons";
+import { MoonIcon, SunIcon } from "../icons";
 
 interface ButtonToggleThemeProps {
     theme: string
@@ -9,20 +9,41 @@ export default function ButtonToggleTheme(props: ButtonToggleThemeProps) {
 
     return props.theme === 'dark' ? (
         <div onClick={props.toggleTheme} className={`
-        
+            hidden sm:flex items-center cursor-pointer
+            bg-gradient-to-r from-yellow-300 to-yellow-600
+            w-14 lg:w-24 h-8 p-1 rounded-full 
         `}>
             <div className={`
-            
+                flex items-center justify-center
+                bg-white text-yellow-600 w-6 h-6 rounded-full
             `}>
-                {SunIcon}
+                {SunIcon(5)}
             </div>
             <div className={`
-            
+                hidden lg:flex items-center ml-4
+                text-white
             `}>
-                <span>Light</span>
+                <span className="text-sm">Light</span>
             </div>
         </div>
     ) : (
-        <div></div>
+        <div onClick={props.toggleTheme} className={`
+            hidden sm:flex items-center justify-end cursor-pointer
+            bg-gradient-to-r from-gray-500 to-gray-900
+            w-14 lg:w-24 h-8 p-1 rounded-full 
+        `}>
+            <div className={`
+                hidden lg:flex items-center mr-4
+                text-gray-300
+            `}>
+                <span className="text-sm">Dark</span>
+            </div>
+            <div className={`
+                flex items-center justify-center
+                bg-black text-yellow-300 w-6 h-6 rounded-full
+            `}>
+                {MoonIcon(5)}
+            </div>
+        </div>
     )
 }
