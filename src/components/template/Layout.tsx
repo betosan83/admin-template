f6a16e6f75852eef5170b8d1fd43fd17d4364494
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-11-12 06:05:48 -0300

    Refactoring Force auth as a component

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,6 +1,6 @@
 import useAppData from "../../data/hook/useAppData";
 import Content from "./Content";
-import ForceAuth from '../auth/ForceAuth'
+import ForceAuth from '../../functions/forceAuth'
 import SideMenu from "./SideMenu";
 import TopBar from "./TopBar";
 
@@ -13,8 +13,7 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
     const { theme } = useAppData()
 
-    return (
-        <ForceAuth>
+    return ForceAuth (
             <div className={`${theme} flex h-screen w-screen`}>
                 <SideMenu/>
                 <div className={`flex flex-col w-full p-7 
@@ -25,6 +24,5 @@ export default function Layout(props: LayoutProps) {
                     </Content>
                 </div>
             </div>
-        </ForceAuth>
     )
 }
